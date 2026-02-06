85adcec9de1a583566ef491d91bef0e8a58ba612
dscccgujrat9898-blip <dscccgujrat9898@gmail.com>
2026-02-05 23:07:10 -0800

    Add model URLs for u2net and modnet

--- app.js
+++ app.js
@@ -45,7 +45,9 @@ const INPUT_SIZE = 320;
 // u2net + modnet should be in GitHub Releases (paste direct asset URLs)
 const MODEL_URLS = {
   u2netp: "./models/u2netp.onnx",
+
   u2net: "https://huggingface.co/dsccckadodara/ds-bg-models/raw/main/u2net.onnx",
+
   modnet: "https://huggingface.co/dsccckadodara/ds-bg-models/raw/main/model.onnx",
 };
 
